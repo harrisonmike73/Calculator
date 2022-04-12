e2dcdc4baf6a32aae404774ee286ee43609d5fd4
Michael Harrsion <harrisonmike73@yahoo.com>
2022-04-12 14:49:49 -0500

    fixed bugs

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ class Calculator {
     }
 
     clear() {
-        this.currentOperand = '';
-        this.previousOperand = '';
-        this.operation = undefined;
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete() {
-
+        // converts all currentOperand numbers to a string except for the last number, 
+        //(deleting the last entered number)
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     // add a number to the screen
@@ -25,6 +27,12 @@ class Calculator {
     }
 
     chooseOperation(operation) {
+        // exits code if the currentoperand is empty
+        if (this.currentOperand === '') return
+        // if display already contains a previous number and a current number, compute those numbers
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
         // sets operation to what is passed in
         this.operation = operation
         // moves the current op to the previous op
@@ -36,13 +44,77 @@ class Calculator {
 
 
     compute() {
+        let computation
+        // converts string to a number
+        const prev = parseFloat(this.previousOperand)
+        // converts string to a number
+        const current = parseFloat(this.currentOperand)
+        // will not run code if equals is clicked before entering numbers
+        if (isNaN(prev) || isNaN(current)) return
+        // basic maths for operations
+        switch(this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+                // breaks out of code without computing if invalid operation
+            default: 
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
 
     }
+    getDisplayNumber(number) {
+        // splits the number and every number after the decimal place
+        const stringNumber = number.toString()
+        // takes string and converts into an array of 2 parts. --
+        //Everything before the decimal and everything after the decimal
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        // checks if no numbers are inputed or just a decimal "."
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        // checks if numbers ARE inputed
+        } else {
+        // displays numbers that are entered
+          integerDisplay = integerDigits.toLocaleString('en', 
+        // never any decimal places after the integerDigits value once its converted to a string with commas
+          { maximumFractionDigits: 0 })
+        }
+        // runs if user entered a decimal and has numbers after it
+        if (decimalDigits != null) {
+        // displays numbers with decimal and the digits after decimal
+          return `${integerDisplay}.${decimalDigits}`
+        // runs if there are NO digits after decimal
+        } else {
+          return integerDisplay
+        }
+      }
+    
 
     updateDisplay() {
         // displays operands
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = 
+            this.getDisplayNumber(this.currentOperand)
+        // appends the operation sign to the end of previousOperand
+        if(this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        // clears the previousOperand value once computed        
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
@@ -59,7 +131,7 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-
+// func for all numbers
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
@@ -68,10 +140,29 @@ numberButtons.forEach(button => {
     })
 })
 
+// func for all operations
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
         // updates display when an operation button is clicked
         calculator.updateDisplay()
     })
+})
+
+// runs compute and updatesDisplay function upon clicking "="
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+// clears display, all operands and operations upon clicking "AC"
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+// removes last entered number from display
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
